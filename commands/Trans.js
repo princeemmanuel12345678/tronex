5e92f9fc311556236b9cd4f8a80fdeec462cc7ce
princeemmanuel12345678 <76397766+princeemmanuel12345678@users.noreply.github.com>
2021-07-19 22:43:44 +0100

    Add files via upload

---
+++ commands/Trans.js
@@ -0,0 +1,39 @@
+/*CMD
+  command: Trans
+  help: 
+  need_reply: true
+  auto_retry_time: 
+  folder: 
+  answer: 
+  keyboard: 
+  aliases: 
+CMD*/
+
+BBAdmin.installBot(
+
+{ email: 'princeamadi7007@gmail.com',
+
+// see bot id in the app -> Bots -> Bot
+
+bot_id: 379301,
+
+// bot can be installed as protected
+
+//as_protected: true,
+
+// you can pass properties to bot:
+
+// bot_properties: [
+
+// { name: 'test',
+
+// value:'hello world',
+
+// type:'string' }
+
+// ]
+
+}
+
+)
+Bot.sendMessage("Transferred")
